b84f6ccf491d91066307dae00c7f1153947be44a
murilobarbosa2002 <murilo.barbosa2609@gmail.com>
2022-06-17 10:39:18 -0300

    Edit Routers and App.

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ export function AppRouter(){
     return(
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/docs" element={<DocsPage />} />
+            <Route path="docs" element={<DocsPage />} />
         </Routes>
     )
 }
