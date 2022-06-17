8273afcc4db40e5a1fb4cd792bd030ed184ce172
murilobarbosa2002 <murilo.barbosa2609@gmail.com>
2022-06-17 11:10:10 -0300

    Edit for vite project

--- documentation-site-reactjs/src/App.tsx
+++ documentation-site-reactjs/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AppRouter } from './routers/router'
-function App() {
+export function App() {
   return (
     <Router>
       <AppRouter />
