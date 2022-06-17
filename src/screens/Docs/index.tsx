9f2f9548ae838980de6166b1e771461b823ba51d
murilobarbosa2002 <murilo.barbosa2609@gmail.com>
2022-06-17 11:18:53 -0300

    Edits

--- src/screens/Docs/index.tsx
+++
@@ -1,7 +0,0 @@
-export function DocsPage(){
-    return(
-        <h1 className="text-3xl font-bold underline">
-            Hello world 2!
-        </h1>
-    )
-}
--- src/screens/Home/index.tsx
+++
@@ -1,7 +0,0 @@
-export function HomePage(){
-    return(
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
-}
